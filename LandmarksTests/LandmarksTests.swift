6c3a073bbf9d3baeffb473a5fb4bea0c113fa5dd
Captaintom14 <60295341+Captaintom14@users.noreply.github.com>
2024-12-11 12:17:47 -0800

    Initial Commit

---
+++ LandmarksTests/LandmarksTests.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksTests.swift
+//  LandmarksTests
+//
+//  Created by Thomas Mejia on 2024-12-11.
+//
+
+import Testing
+@testable import Landmarks
+
+struct LandmarksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
